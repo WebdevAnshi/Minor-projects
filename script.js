857ac078c326255ef57f1ab138ff59a445964f8f
Anshika <manshika515@gmail.com>
2024-07-22 18:56:57 +0530

    Add files via upload
    Used gsap for animation.

---
+++ script.js
@@ -0,0 +1,98 @@
+gsap.to(".box", {
+    x:444,
+    y:180,
+    backgroundColor: "blue",
+    duration: 5,
+    // repeat: 5,
+    borderRadius: "50%",
+    delay: 0.1,
+    yoyo: true,
+})
+
+gsap.to(".box2", {
+    x:-620,
+    y:212,
+    backgroundColor: "blue",
+    duration: 5,
+    // repeat: 5,
+    borderRadius: "50%",
+    delay: 0.1,
+    yoyo: true,
+})
+
+gsap.to(".box3", {
+    x:-512,
+    y:-245,
+    backgroundColor: "blue",
+    duration: 5,
+    // repeat: 5,
+    borderRadius: "50%",
+    delay: 0.1,
+    yoyo: true,
+})
+
+gsap.to(".box4", {
+    x:-360,
+    y: -100,
+    backgroundColor: "blue",
+    duration: 5,
+    // repeat: 5,
+    borderRadius: "50%",
+    delay: 0.1,
+    yoyo: true,
+})
+
+gsap.to(".box5", {
+    x: 663,
+    y: 15,
+    backgroundColor: "blue",
+    duration: 5,
+    // repeat: 5,
+    borderRadius: "50%",
+    delay: 0.1,
+    yoyo: true,
+})
+
+gsap.to(".box6", {
+    x: 890,
+    y: -5,
+    backgroundColor: "blue",
+    duration: 5,
+    // repeat: 5,
+    // borderRadius: "50%",
+    delay: 1,
+    yoyo: true,
+})
+
+gsap.to(".box7", {
+    y: -355,
+    x: -5,
+    backgroundColor: "blue",
+    duration: 5,
+    // repeat: 5,
+    // borderRadius: "50%",
+    delay: 2,
+    yoyo: true,
+})
+
+gsap.to(".box8", {
+    x: -890,
+    y: 5,
+    backgroundColor: "blue",
+    duration: 5,
+    // repeat: 5,
+    // borderRadius: "50%",
+    delay: 3,
+    yoyo: true,
+})
+
+gsap.to(".box9", {
+    y: 310,
+    x: 5,
+    backgroundColor: "blue",
+    duration: 5,
+    // repeat: 5,
+    // borderRadius: "50%",
+    delay: 4,
+    yoyo: true,
+})
